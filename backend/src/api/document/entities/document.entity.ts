1d388d84268a9295ddf33fbd470b756108d1082a
우준성 <woojs0505@naver.com>
2022-05-04 12:11:54 +0900

    :memo: :: document 엔티티 작성

--- backend/src/api/document/entities/document.entity.ts
+++ backend/src/api/document/entities/document.entity.ts
@@ -1,1 +1,30 @@
-export class Document {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('document')
+export class Document {
+  @PrimaryGeneratedColumn({
+    name: 'idx',
+  })
+  idx!: number;
+
+  @Column({
+    name: 'title',
+  })
+  title!: string;
+
+  @Column({
+    name: 'text',
+    type: 'mediumtext',
+  })
+  text!: string;
+
+  @CreateDateColumn({
+    name: 'created_at',
+  })
+  createdAt!: Date;
+}
