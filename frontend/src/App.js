8036f2bbe75dd8645f8b16a198c329cb1b5f3563
우준성 <woojs0505@naver.com>
2022-05-13 08:49:22 +0900

    :memo: :: routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,20 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/footer/Footer";
 import Main from "./components/main/Main";
 import Navbar from "./components/navbar/Navbar";
+import NotFound from "./components/notFound/NotFound";
+import Detail from "./components/detail/Detail";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Main />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/detail/*" element={<Detail />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
       <Footer />
     </div>
   );
---
+++ frontend/src/components/detail/Detail.js
@@ -0,0 +1,9 @@
+function Detail() {
+  return (
+    <div>
+      <h1>하하</h1>
+    </div>
+  );
+}
+
+export default Detail;
