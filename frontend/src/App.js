72871f19a488e6b263cfb548ee8033c3472e3723
우준성 <woojs0505@naver.com>
2022-05-12 22:58:03 +0900

    🚧 :: change frontend to javascript

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,24 @@
-import styled from "styled-components";
-
-const Father = styled.div`
-  display: flex;
-`;
-
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
-`;
-
-const Circle = styled(Box)`
-  border-radius: 50px;
-`;
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Father>
-      <Box bgColor="teal" />
-      <Box bgColor="tomato" />
-      <Circle bgColor="beige" />
-    </Father>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
