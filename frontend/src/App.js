cd88ba24851c3f6f21528369ab50ceac560a336d
우준성 <woojs0505@naver.com>
2022-05-13 00:09:54 +0900

    :memo: :: html and css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="navbar">
+        <h1 className="navbar-title">경태위키</h1>
+        <ul className="navbar-items">
+          <li className="navbar-item">인물</li>
+          <li className="navbar-item">사건사고</li>
+          <li className="navbar-item">프로젝트에 관해</li>
+        </ul>
+      </div>
+      <div className="main">
+        <div className="main-jumbotron">
+          <h4 className="main-h4">
+            지금, 경태에 대해서 더 알아볼 수 있는 기회
+          </h4>
+          <h2 className="main-h2">경태위키</h2>
+        </div>
+        <div className="list-item">
+          <h4 className="list-item-title">기숙사 입소 첫 날의 경태</h4>
+          <p className="list-item-p">조회수 101</p>
+        </div>
+        <div className="list-item">
+          <h4 className="list-item-title">기숙사 입소 첫 날의 경태</h4>
+          <p className="list-item-p">조회수 101</p>
+        </div>
+        <div className="list-item">
+          <h4 className="list-item-title">기숙사 입소 첫 날의 경태</h4>
+          <p className="list-item-p">조회수 101</p>
+        </div>
+      </div>
+      <div className="footer">
+        <ul className="footer-menu">
+          <li>
+            <a href="/">공지사항</a>
+          </li>
+          <li>
+            <a href="/">개인정보처리방침</a>
+          </li>
+          <li>
+            <a href="/">이용약관</a>
+          </li>
+          <li>
+            <a href="/">자료 출처</a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
