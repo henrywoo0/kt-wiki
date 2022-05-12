b58a9765acd5e4b0c9c1fa01e16ede0fe0883b6c
우준성 <woojs0505@naver.com>
2022-05-12 13:27:38 +0900

    :pencil2: :: study styled component

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+const Father = styled.div`
+  display: flex;
+`;
+
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
+  width: 100px;
+  height: 100px;
+`;
+
+const Circle = styled(Box)`
+  border-radius: 50px;
+`;
+
+function App() {
+  return (
+    <Father>
+      <Box bgColor="teal" />
+      <Box bgColor="tomato" />
+      <Circle bgColor="beige" />
+    </Father>
+  );
+}
+
+export default App;
