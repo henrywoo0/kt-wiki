ab1ff4b2d5f2bfa1585bc0b20b894a3d29665de5
우준성 <woojs0505@naver.com>
2022-05-13 18:16:44 +0900

    :memo: :: Main.js, Navbar.js

--- frontend/src/components/main/Main.js
+++ frontend/src/components/main/Main.js
@@ -1,8 +1,21 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import Item from "../item/Item";
 import Jumbotron from "../jumbotron/Jumbotron";
 import "./Main.css";
 
 function Main() {
+  const [documents, setDocuments] = useState([]);
+
+  const getDocuments = async () => {
+    const json = await axios.get(
+      `${process.env.REACT_APP_SERVER_URL}/document/`
+    );
+    setDocuments(json.data.documents);
+  };
+
+  useEffect(() => {});
+
   return (
     <div className="main">
       <Jumbotron />
--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -3,7 +3,9 @@ import "./Navbar.css";
 function Navbar() {
   return (
     <div className="navbar">
-      <h1 className="navbar-title">경태위키</h1>
+      <h1 className="navbar-title">
+        <a href="/">경태위키</a>
+      </h1>
       <ul className="navbar-items">
         <li className="navbar-item">인물</li>
         <li className="navbar-item">사건사고</li>
