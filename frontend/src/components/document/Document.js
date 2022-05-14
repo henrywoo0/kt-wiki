05c300dff4c53fe957693b9767ad195f4cb5e9f2
우준성 <woojs0505@naver.com>
2022-05-14 22:18:02 +0900

    :memo: :: document component

--- frontend/src/components/document/Document.js
+++ frontend/src/components/document/Document.js
@@ -1,7 +1,9 @@
 import axios from "axios";
+import moment from "moment";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Markdown from "../markdown/Markdown";
+import "./Document.css";
 
 function Document() {
   const { idx } = useParams();
@@ -19,8 +21,17 @@ function Document() {
   }, []);
 
   return (
-    <div>
+    <div className="document">
       <h2 className="document-title">{document.title}</h2>
+      <div className="document-properties">
+        <p className="document-p">조회수 {document.hits}</p>
+        <p className="document-p">
+          {moment(document.createdAt).format("YYYY년 MM월 DD일 hh:mm")} 생성됨
+        </p>
+        <p className="document-p">
+          {moment(document.updatedAt).format("YYYY년 MM월 DD일 hh:mm")} 수정됨
+        </p>
+      </div>
       <Markdown text={document.text} />
     </div>
   );
