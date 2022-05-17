43cb444b5b979b2511ec19e06d516ecc253891e9
우준성 <woojs0505@naver.com>
2022-05-17 22:39:00 +0900

    :memo: :: login ui

--- frontend/src/components/login/Login.js
+++ frontend/src/components/login/Login.js
@@ -1,13 +1,29 @@
+import { Link } from "react-router-dom";
 import "./Login.css";
 
 function Login() {
   return (
     <div className="login">
       <form>
-        <input type="text" name="id" id="id" placeholder="아이디" />
-        <input type="password" name="id" id="id" placeholder="패스워드" />
-        <input type="submit" value="제출" />
+        <input
+          type="text"
+          name="id"
+          id="id"
+          placeholder="아이디"
+          className="login-input"
+        />
+        <input
+          type="password"
+          name="id"
+          id="id"
+          placeholder="패스워드"
+          className="login-input"
+        />
+        <input type="submit" value="제출" className="login-input-submit" />
       </form>
+      <Link to="/signup">
+        <p className="login-text">아직 계정이 없으신가요?</p>
+      </Link>
     </div>
   );
 }
